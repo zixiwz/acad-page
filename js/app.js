df97e6e27e1bde853b69dc14e1394c8e9b32803b
Wang Zixi <393669798@qq.com>
2025-02-07 20:23:52 +0800

    [更新]优化样式，添加新颜色变量，调整出版物列表，增强链接交互性

--- js/app.js
+++ js/app.js
@@ -98,6 +98,9 @@ class App {
 		if (pageId === "links") {
 			addEventListenerToLinks();
 		}
+		if (pageId === "publications") {
+			addEventListenerToPublications();
+		}
 	}
 }
 
@@ -119,6 +122,24 @@ function addEventListenerToLinks() {
 			webDisplay.src = event.target.dataset.url || "#";
 		});
 	}
+}
+
+function addEventListenerToPublications() {
+	console.log('加载publications.html');
+	const listItems = document.querySelectorAll("ol li");
+	console.log(listItems);
+	listItems.forEach(function (listItem) {
+		listItem.addEventListener('click', function () {
+			const title = this.querySelector('p strong').textContent;
+			navigator.clipboard.writeText(title)
+				.then(() => {
+					console.log('标题已复制到剪贴板: ', title);
+				})
+				.catch((err) => {
+					console.error('复制标题失败: ', err);
+				});
+		});
+	});
 }
 
 function setLanguageVisibility(currentLanguage) {
