56ce2cb3b52d46130231afd4feecbdeaff908228
Wang Zixi <393669798@qq.com>
2025-02-16 18:50:48 +0800

    [更新]在链接页面添加一句话描述，优化链接项的样式和交互性

--- js/app.js
+++ js/app.js
@@ -111,6 +111,7 @@ function addEventListenerToLinks() {
 	const favicon = document.getElementById("favicon");
 	const siteName = document.getElementById("site-name");
 	const siteLink = document.getElementById("site-link");
+	const siteSent = document.getElementById("one-sentence");
 	const webDisplay = document.getElementById("web-display");
 
 	for (let i = 0; i < linkList.length; i++) {
@@ -119,6 +120,7 @@ function addEventListenerToLinks() {
 			favicon.src = event.target.dataset.avatar || "images/default-avatar.svg";
 			siteName.textContent = event.target.textContent;
 			siteLink.href = event.target.dataset.url;
+			siteSent.textContent = event.target.dataset.sentence;
 			webDisplay.src = event.target.dataset.url || "#";
 		});
 	}
